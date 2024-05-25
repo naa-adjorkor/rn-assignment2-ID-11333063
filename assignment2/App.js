3d0d68b39fcc7e8a4a600bb5a7a24e461d7ae8a5
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-05-25 01:31:14 +0000

    font bold

--- assignment2/App.js
+++ assignment2/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Naa Adjorkor Akrong Adjei</Text>
+      <Text style={styles.text}>
+        My name is <Text style={styles.bold}>Naa Adjorkor Akrong Adjei</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +21,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });

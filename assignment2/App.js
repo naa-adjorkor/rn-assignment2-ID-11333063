bb5a32bee688113201a3337949ffe3829a425459
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-05-25 01:24:34 +0000

    bg change

--- assignment2/App.js
+++ assignment2/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f6b7',
     alignItems: 'center',
     justifyContent: 'center',
   },
